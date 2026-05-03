1d0ed6d3d4d25ecdaa8008fe71d71ea6c38e53e7
lianamaya022 <lianamaya022@gmail.com>
2026-05-03 13:45:29 +0200

    Opsætning af HTML, CSS og JS

---
+++ javascript/liana.js
@@ -0,0 +1,17 @@
+//krav hele pensum
+
+//JavaScript.
+//Placering og udførelse af JS.
+//Fejlfinding.
+//Variabler og typer.
+//Operatorer.
+//Arrays.
+//Kontrolstruktur (if-Else).
+//Kontrolstruktur (loops).
+//Objekter.
+//DOM (Document Object Model).
+//Funktioner (mere end halvejs).
+//Variable scoop (herunder LET).
+//Events.
+//Libraries.
+//BEM.
